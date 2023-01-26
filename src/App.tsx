acd11c0d9a7248cebec2064ba5cb28b5ce410741
Casper Andreassen <caspersa@stud.ntnu.no>
2023-01-26 18:41:08 +0100

    Started work on navbar and added projects page. Added linting and prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
+import Projects from './pages/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Main />}>
+        <Route path="/projects" element={<Projects />}></Route>
+        <Route path="/contact"></Route>
+      </Route>
+    </Routes>
   );
 }
 
